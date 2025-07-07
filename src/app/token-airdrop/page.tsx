b1e39c899c850ae508899f9d01e573258f6e4ca5
Tonashiro <vinit.tonashiro@hotmail.com>
2025-07-06 23:09:43 -0300

    feat: add coming soon pages

---
+++ src/app/token-airdrop/page.tsx
@@ -0,0 +1,9 @@
+export default function TokenAirdropPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl sm:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary text-center p-3">
+        Coming soon
+      </h1>
+    </div>
+  );
+}
